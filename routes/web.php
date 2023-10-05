924ec23e16f59e75bfb940d5d3ab9c481b422884
MD. Faysal Rahman <faysal35-3119@diu.edu.bd>
2023-10-05 19:35:34 +0600

    test

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,11 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+require __DIR__ . '/auth.php';
